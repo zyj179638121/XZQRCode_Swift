1631580bd690cb86386a1e0b38ea4060b5eb940e
zhaoyongjie <18515381796@163.com>
2017-01-06 14:05:39 +0800

    Initial Commit

---
+++ XZQRCode_Swift/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  XZQRCode_Swift
+//
+//  Created by MYKJ on 17/1/6.
+//  Copyright © 2017年 zhaoyongjie. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
